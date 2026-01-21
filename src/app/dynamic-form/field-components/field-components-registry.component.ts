042e0aa2965f35c6cf87963aff4fedfda94d36fc
ANTONIO IUELE <antonio.iuele@esternibisp.com>
2026-01-21 12:29:22 +0100

    refactor: use runInInjectionContext to retrieve control type from components
    - Update FieldComponentsRegistryComponent to use runInInjectionContext() for proper injection context
    - Remove static CONTROL_TYPE properties from all input components
    - Use getControlType() method via component instantiation in injection context
    - Fixes NG0203 injection context errors when creating component instances

--- src/app/dynamic-form/field-components/field-components-registry.component.ts
+++ src/app/dynamic-form/field-components/field-components-registry.component.ts
@@ -1,4 +1,4 @@
-import { Component, inject, OnInit } from '@angular/core';
+import { Component, inject, Injector, OnInit, runInInjectionContext } from '@angular/core';
 import { FieldComponentRegistryService } from './field-component-registry.service';
 import { TextInputComponent } from './text-input/text-input';
 import { EmailInputComponent } from './email-input/email-input';
@@ -9,6 +9,7 @@ import { TextareaInputComponent } from './textarea-input/textarea-input';
 import { SelectInputComponent } from './select-input/select-input';
 import { RadioInputComponent } from './radio-input/radio-input';
 import { CheckboxInputComponent } from './checkbox-input/checkbox-input';
+import { BaseFieldComponent, ControlType } from './base-field.component';
 
 /**
  * Library component that registers all field components in the FieldComponentRegistryService
@@ -33,17 +34,29 @@ import { CheckboxInputComponent } from './checkbox-input/checkbox-input';
 })
 export class FieldComponentsRegistryComponent implements OnInit {
   private registry = inject(FieldComponentRegistryService);
+  private injector = inject(Injector);
 
   ngOnInit(): void {
-    // Register all field components
-    this.registry.register('text', TextInputComponent);
-    this.registry.register('email', EmailInputComponent);
-    this.registry.register('number', NumberInputComponent);
-    this.registry.register('password', PasswordInputComponent);
-    this.registry.register('date', DateInputComponent);
-    this.registry.register('textarea', TextareaInputComponent);
-    this.registry.register('select', SelectInputComponent);
-    this.registry.register('radio', RadioInputComponent);
-    this.registry.register('checkbox', CheckboxInputComponent);
+    // Register all field components with their control types
+    // Use runInInjectionContext to properly instantiate components and call getControlType()
+    this.registry.register('text', TextInputComponent, this.getControlType(TextInputComponent));
+    this.registry.register('email', EmailInputComponent, this.getControlType(EmailInputComponent));
+    this.registry.register('number', NumberInputComponent, this.getControlType(NumberInputComponent));
+    this.registry.register('password', PasswordInputComponent, this.getControlType(PasswordInputComponent));
+    this.registry.register('date', DateInputComponent, this.getControlType(DateInputComponent));
+    this.registry.register('textarea', TextareaInputComponent, this.getControlType(TextareaInputComponent));
+    this.registry.register('select', SelectInputComponent, this.getControlType(SelectInputComponent));
+    this.registry.register('radio', RadioInputComponent, this.getControlType(RadioInputComponent));
+    this.registry.register('checkbox', CheckboxInputComponent, this.getControlType(CheckboxInputComponent));
+  }
+
+  /**
+   * Creates a component instance in injection context and retrieves its control type
+   */
+  private getControlType<T extends BaseFieldComponent>(componentType: new (...args: any[]) => T): ControlType {
+    return runInInjectionContext(this.injector, () => {
+      const instance = new componentType();
+      return instance.getControlType();
+    });
   }
 }
